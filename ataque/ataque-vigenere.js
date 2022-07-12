439523133bc5621326e44e2b7632d8221ba5fa46
= <carlosedsb0110@gmail.com>
2022-07-12 00:41:52 -0300

    implementa ataque

--- ataque/ataque-vigenere.js
+++ ataque/ataque-vigenere.js
@@ -9,16 +9,16 @@ function calcularSequenciasRepetidas(texto) {
         let sequencia = texto.substring(i, i + 3);
 
         for(let j = i + 3; j < texto.length; j++) {
-            let sequenciaInterna = texto.substring(j, j+3)
+            let sequenciaInterna = texto.substring(j, j + 3)
 
             if(sequencia === texto.substring(j, j + 3)) {
                 sequencias.push([sequenciaInterna, j - i]);
             }
         }
     }
-    
-    sequencias.map(numero => {
-        let divisores = retornaDivisores(numero[1]);
+
+    sequencias.map(sequencia => {
+        let divisores = retornaDivisores(sequencia[1]);
 
         divisores.map(divisor => {
             ocorrencias[divisor] ? ocorrencias[divisor]++ : ocorrencias[divisor] = 1;
@@ -36,7 +36,7 @@ function retornaDivisores(numero){
 	for(var i = 2; i <= TAMANHO_MAXIMO_CHAVE; i++){
         numero % i === 0 ? divisores.push(i) : ''
 	}
-    
+
 	return divisores;
 }
 
@@ -60,32 +60,32 @@ function eventoDOMCalcularFrequencia(event) {
 }
 
 const frequencia_ingles = {
-    'a':8.167,
-    'b':1.492,
-    'c':2.782,
-    'd':4.253,
-    'e':12.702,
-    'f':2.228,
-    'g':2.015,
-    'h':6.094,
-    'i':6.966,
-    'j':0.153,
-    'k':0.772,
-    'l':4.025,
-    'm':2.406,
-    'n':6.749,
-    'o':7.507,
-    'p':1.929,
-    'q':0.095,
-    'r':5.987,
-    's':6.327,
-    't':9.056,
-    'u':2.758,
-    'v':0.978,
-    'w':2.360,
-    'x':0.150,
-    'y':1.974,
-    'z':0.074
+    'a': 8.167,
+    'b': 1.492,
+    'c': 2.782,
+    'd': 4.253,
+    'e': 12.702,
+    'f': 2.228,
+    'g': 2.015,
+    'h': 6.094,
+    'i': 6.966,
+    'j': 0.153,
+    'k': 0.772,
+    'l': 4.025,
+    'm': 2.406,
+    'n': 6.749,
+    'o': 7.507,
+    'p': 1.929,
+    'q': 0.095,
+    'r': 5.987,
+    's': 6.327,
+    't': 9.056,
+    'u': 2.758,
+    'v': 0.978,
+    'w': 2.360,
+    'x': 0.150,
+    'y': 1.974,
+    'z': 0.074
 }
 
 
@@ -106,4 +106,3 @@ for(let letra of Object.entries(frequencia_ingles)) {
     </td>
     `
 }
-
